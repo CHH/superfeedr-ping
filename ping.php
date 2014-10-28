c40a9b938cba3a37dbafd84283bb0144640c77c3
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2014-10-28 14:56:33 +0100

    Remove unused variable

--- ping.php
+++ ping.php
@@ -24,8 +24,6 @@ function heroku_log($string, $variables = [])
     fwrite($log, $string."\n");
 }
 
-$feedUrl = @$_SERVER['FEED_URL'] ?: 'http://christophh.net/atom.xml';
-
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('HTTP/1.1 403 Forbidden');
     return;
